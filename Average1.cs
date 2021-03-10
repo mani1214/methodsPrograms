9bebc73577ce5a9d28831f9a1b7d96881584caf6
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-03-10 11:00:26 +0530

    Add files via upload

---
+++ Average1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Average1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(52423));
+        }
+        public static int Average(int num)
+        {
+            int sum = 0;
+            int count = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                sum = sum + digit;
+                count++;
+                num /= 10;
+            }
+            int avg = sum / count;
+            return avg;
+
+
+        }
+    }
+}
