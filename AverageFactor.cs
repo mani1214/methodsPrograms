c207ae0261c4629dd513c075235e718110a5cf85
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-03-10 10:55:56 +0530

    Add files via upload

---
+++ AverageFactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class AverageFactor
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(AverageOfFactors(12));
+        }
+        public static int AverageOfFactors(int num)
+        {
+            int count = 0;
+            int sum = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                    count++;
+                }
+            }
+            int avg = sum / count;
+            return avg;
+        }
+    }
+}
