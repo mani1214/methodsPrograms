5c2abbd53a205b8a97ded30a9c16b7e04e96f7db
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-03-10 11:02:56 +0530

    Add files via upload

---
+++ MaxNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class MaxNum
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(MaxDigitNumber(292617));
+        }
+        public static int MaxDigitNumber(int num)
+        {
+            int max = num % 10;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                if (digit > max)
+                {
+                    max = digit;
+                }
+                num /= 10;
+            }
+            return max;
+        }
+    }
+}
